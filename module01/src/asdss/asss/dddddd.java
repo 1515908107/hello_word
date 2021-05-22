c90df4e83bd9e99f91d72594a316ec6635ebabdf
1515908107 <1515908107@qq.com>
2021-05-22 17:56:32 +0800

    Default Changelist

---
+++ module01/src/asdss/asss/dddddd.java
@@ -0,0 +1,18 @@
+package asdss.asss;
+
+public class dddddd {
+
+    public static void main(String [] args){
+        System.out.println("hello word");
+
+    }
+
+
+    /*
+    
+     */
+    public void sas(){
+
+    }
+
+}
---
+++ module01/src/asdss/asss/waaa.java
@@ -0,0 +1,9 @@
+package asdss.asss;
+
+/**
+ * @author WUjinron
+ * @create 2021-05-19 23:31
+ **/
+
+public class waaa {
+}
